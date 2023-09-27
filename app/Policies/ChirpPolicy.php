905ac0f34d6a6153042f69b99656a02f680356a9
rzfhlv <rzfhlv.me@gmail.com>
2023-09-27 15:19:16 +0700

    Deleting Chirps

--- app/Policies/ChirpPolicy.php
+++ app/Policies/ChirpPolicy.php
@@ -9,30 +9,6 @@ use Illuminate\Auth\Access\Response;
 class ChirpPolicy
 {
     /**
-     * Determine whether the user can view any models.
-     */
-    // public function viewAny(User $user): bool
-    // {
-    //     //
-    // }
-
-    /**
-     * Determine whether the user can view the model.
-     */
-    // public function view(User $user, Chirp $chirp): bool
-    // {
-    //     //
-    // }
-
-    /**
-     * Determine whether the user can create models.
-     */
-    // public function create(User $user): bool
-    // {
-    //     //
-    // }
-
-    /**
      * Determine whether the user can update the model.
      */
     public function update(User $user, Chirp $chirp): bool
@@ -43,24 +19,8 @@ class ChirpPolicy
     /**
      * Determine whether the user can delete the model.
      */
-    // public function delete(User $user, Chirp $chirp): bool
-    // {
-    //     //
-    // }
-
-    /**
-     * Determine whether the user can restore the model.
-     */
-    // public function restore(User $user, Chirp $chirp): bool
-    // {
-    //     //
-    // }
-
-    /**
-     * Determine whether the user can permanently delete the model.
-     */
-    // public function forceDelete(User $user, Chirp $chirp): bool
-    // {
-    //     //
-    // }
+    public function delete(User $user, Chirp $chirp): bool
+    {
+        return $this->update($user, $chirp);
+    }
 }
