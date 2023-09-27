601098aae54c4ece82d0712671de2e559c56f1e3
rzfhlv <rzfhlv.me@gmail.com>
2023-09-27 15:06:17 +0700

    Showing Chirps

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -16,7 +16,7 @@ class ChirpController extends Controller
     public function index(): Response
     {
         return Inertia::render('Chirps/Index', [
-
+            'chirps' => Chirp::with('user:id,name')->latest()->get(),
         ]);
     }
 
@@ -38,7 +38,7 @@ class ChirpController extends Controller
         ]);
 
         $request->user()->chirps()->create($validated);
-        
+
         return redirect(route('chirps.index'));
     }
 
